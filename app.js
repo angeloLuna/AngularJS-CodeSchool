dd9508ffc4d5147ee71c29fdd24babff45e2b336
angeloLuna <angel.luna.najera@gmail.com>
2016-04-06 15:46:59 -0500

    filto con ng-repeat para mostrar un valor con simbolo de moneda usando | currency

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
 angular.module('gemStore', [])
 .controller('StoreController', controller);
 
-var gem = {
-    name: 'Azurite', 
-    price: 2.95,
-    canPurchase:true,
-    soldOut: true, };
+var gems = [
+    { name: 'Azurite', price: 110.50 },
+    { name: 'Bloodstone', price: 22.90 },
+    { name: 'Zircon', price: 1100 },
+];
 
 function controller(){
     var first = this;
-    first.product =gem;
+    first.product =gems;
 }
 
 })();
