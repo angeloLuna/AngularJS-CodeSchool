308203d40224f4170a8a812647e941c1fcc05b5a
angeloLuna <angel.luna.najera@gmail.com>
2016-04-22 17:58:57 -0500

    se agregaron imagenes en forma de una galeria con un ng-repeat y agregando una variable image en el ng-repeat

--- app.js
+++ app.js
@@ -5,9 +5,46 @@ angular.module('gemStore', [])
 .controller('StoreController', controller);
 
 var gems = [
-    { name: 'Azurite', price: 110.50 },
-    { name: 'Bloodstone', price: 22.90 },
-    { name: 'Zircon', price: 1100 },
+    {
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 10.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [
+      "img/img1.jpg",
+      "img/img2.jpg",
+      "img/img3.jpg"
+    ]
+  }, {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
+    images: [
+      "img/img2.jpg",
+      "images/gem-03.gif",
+      "images/gem-04.gif"
+    ]
+  }, {
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "images/gem-06.gif",
+      "images/gem-07.gif",
+      "images/gem-10.gif"
+    ]
+  }
 ];
 
 function controller(){
